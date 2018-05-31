f5fe7100b63f924ff103f5ad63fada3ff58022cc
VoTri <tri.vovan@pixta.co.jp>
2018-05-31 09:49:26 +0700

    finish chapter 7

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+      first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -41,7 +41,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com <foo@bar></foo@bar><baz class="c"></baz>om]
+      foo@bar_baz.com <foo@bar></foo@bar><baz class="c"></baz>om]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
